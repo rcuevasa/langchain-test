0550c8635b8fdc6acec3b5d9dc933b1612a56de7
RCA <rodrigo.cuevas.araneda@gmail.com>
2025-12-11 11:32:23 -0300

    Experimenting with langchain and streamlite

---
+++ langchain_helper.py
@@ -0,0 +1,74 @@
+import wikipedia
+
+from langchain_ollama import OllamaLLM
+
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+
+from langchain.agents import create_agent
+from langchain.tools import tool
+
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+    
+
+def langchain_agent():
+    model = OllamaLLM(
+        model="deepseek-r1",
+        base_url="http://localhost:11434",
+        stream=False
+    )
+
+    @tool("tool_wikipedia")
+    def tool_wikipedia(query: str) -> str:
+        """Search a query in wikipedia.
+
+        Args:
+            query: question from the user or llm agent
+        """
+        wikipedia.set_lang("en")
+        # Search for articles
+        search_results = wikipedia.summary(query, sentences=2, max_chars=500)
+        return search_results
+
+    mytools = [tool_wikipedia]
+
+    myagent = create_agent(tools=mytools, model=model)
+
+    result = myagent.invoke({"messages": [{"role": "user", "content": "Who is the president of the United States?"}]})
+
+    print(result)
+
+def generate_pet_names(animal_type, color):
+    prompt_template = PromptTemplate(
+        input_variables=["animal_type", "color"],
+        template='I have a {color} {animal_type} pet, suggest me 10 names for my pet. Deliver all the the pet names in a json array under the key "names".',
+        format="json"
+    )
+	
+    model = OllamaLLM(
+        model="deepseek-r1",
+        base_url="http://localhost:11434",
+        stream=False,
+        #output_key="names"
+        #reasoning=True,
+        format="json"
+    )
+    
+    output_parser = JsonOutputParser()
+
+    #response = llm.generate(model="deepseek-r1", template='prompt_template', format="json")
+    chain = prompt_template | model | output_parser
+
+    response = chain.invoke({"color": color, "animal_type": animal_type})
+
+    return response
+
+#if __name__ == "__main__":
+
+    #print(generate_pet_names('cat', 'black'))
+    #print(langchain_agent())
+    #test_wikipedia()
---
+++ main.py
@@ -0,0 +1,27 @@
+import langchain_helper as lch
+import streamlit as st
+
+st.title("Pet Name Generator")
+
+animal_type = st.sidebar.selectbox("Select Animal Type", ["cat", "dog", "hamster", "parrot", "cow"])
+
+if animal_type == 'cat':
+    pet_color = st.sidebar.text_area("What Color is your Cat?", max_chars=15)
+elif animal_type == 'dog':
+    pet_color = st.sidebar.text_area("What Color is your Dog?", max_chars=15)
+elif animal_type == 'hamster':
+    pet_color = st.sidebar.text_area("What Color is your Hamster?", max_chars=15)
+elif animal_type == 'cow':
+    pet_color = st.sidebar.text_area("What Color is your Cow?", max_chars=15)
+else:
+    pet_color = st.sidebar.text_area("What Color is your Parrot?", max_chars=15)
+
+
+if pet_color:
+    response = lch.generate_pet_names(animal_type, pet_color)
+    st.subheader("Here are some name suggestions for your pet:")
+    #st.text(response)
+    for index,name in enumerate(response['names']):
+        st.write(f"{index + 1}. {name}")
+
+    print(response)
